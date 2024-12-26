901ce4af93bcfc47a9462a0ccffb0c8f1874ebba
seannn9 <naeskirlu9@gmail.com>
2024-12-26 22:54:27 +0800

    added tilt animation on hover for cards

--- frontend/src/components/SkillsCard.jsx
+++ frontend/src/components/SkillsCard.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useRef } from "react";
 import skillCss from "../styles/modules/Skills.module.css";
 import propTypes from "prop-types";
+import VanillaTilt from "vanilla-tilt"; // from https://github.com/micku7zu/vanilla-tilt.js
+
+// Tilt code from: https://codesandbox.io/p/sandbox/vanilla-tilt-with-react-n5ptm?file=%2Fsrc%2Findex.js
+function Tilt(props) {
+    const { options, ...reset } = props;
+    const tilt = useRef(null);
+
+    useEffect(() => {
+        VanillaTilt.init(tilt.current, options);
+    }, [options]);
+    return <div ref={tilt} {...reset} />;
+}
 
 export default function SkillsCard(props) {
+    const options = {
+        scale: 1, // default scale
+        speed: 1000, // default is 300ms
+        max: 15, // default max tilt rotation
+    };
     return (
-        <div className={skillCss.card}>
+        <Tilt className={skillCss.card} options={options}>
             <img src={props.image} alt="" />
             <h2>{props.name}</h2>
-        </div>
+        </Tilt>
     );
 }
 
