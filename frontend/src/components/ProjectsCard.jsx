d9e1e53aab13e2af848a7e889f4d470242e042ad
seannn9 <naeskirlu9@gmail.com>
2024-12-27 00:16:21 +0800

    updated classnames to enable modular css

--- frontend/src/components/ProjectsCard.jsx
+++ frontend/src/components/ProjectsCard.jsx
@@ -1,18 +1,19 @@
 import propTypes from "prop-types";
+import projectCss from "../styles/modules/Projects.module.css";
 
 export default function ProjectsCard(props) {
     return (
         <>
-            <div className="projectCards">
-                <div className="projectRow">
-                    <img className="image" src={props.image} />
-                    <div className="projectInfo">
-                        <h2 className="projectName">{props.name}</h2>
-                        <h3 className="details">{props.details}</h3>
+            <div className={projectCss.projectCards}>
+                <div className={projectCss.projectRow}>
+                    <img className={projectCss.image} src={props.image} />
+                    <div className={projectCss.projectInfo}>
+                        <h2 className={projectCss.projectName}>{props.name}</h2>
+                        <h3 className={projectCss.details}>{props.details}</h3>
                     </div>
                 </div>
                 {props.tech && props.tech.length > 0 && (
-                    <div className="techUsed">
+                    <div className={projectCss.techUsed}>
                         <h4>Technology Used: </h4>
                         {props.tech.map((techImg, index) => (
                             <img key={index} src={techImg} />
